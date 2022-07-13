212c78380392e44218b80b85c8457677dfbeb8eb
galbit_t <galbit_t@etna-alternance.net>
2022-07-13 12:57:45 +0200

    shoot ennemy

--- Assets/WeakSpot.cs
+++ Assets/WeakSpot.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class WeakSpot : MonoBehaviour
 {
+
+    public GameObject objectToDestroy;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Arrow"))
         {
-            Destroy(transform.parent.gameObject);
+            Destroy(objectToDestroy);
         }
     }
 }
